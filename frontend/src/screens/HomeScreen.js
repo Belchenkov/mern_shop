29d40e5845a2bf87376173bfc0f703f6f27875c6
Aleksey Belchenkov <u608110@gmail.com>
2020-10-09 12:41:03 +0300

    Product Details Reducer & Action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -26,19 +26,21 @@ const HomeScreen = () => {
             {
                 loading
                     ? <Loader />
-                    : error
+                : error
                     ? <Message variant='danger'>{ error }</Message>
-                    : (
-                        <Row>
-                            { products.map(product => (
+                : (
+                    <Row>
+                        {
+                            products.map(product => (
                                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                                     <Product
                                         product={product}
                                     />
                                 </Col>
-                            )) }
-                        </Row>
-                    )
+                            ))
+                        }
+                    </Row>
+                )
             }
         </>
     );
