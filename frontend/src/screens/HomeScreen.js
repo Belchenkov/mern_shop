b004f2be155c8c246560befe57fd4dffe2f678bd
Aleksey Belchenkov <u608110@gmail.com>
2020-10-09 12:01:29 +0300

    Loader & Message Error Components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,6 +7,8 @@ import {
 
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -23,9 +25,9 @@ const HomeScreen = () => {
             <h1>Latest Products</h1>
             {
                 loading
-                    ? <h2>Loading...</h2>
+                    ? <Loader />
                     : error
-                    ? <h3>{ error }</h3>
+                    ? <Message variant='danger'>{ error }</Message>
                     : (
                         <Row>
                             { products.map(product => (
