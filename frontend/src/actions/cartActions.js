fb7b7cb2bd4e01b87f4835873e42c3ec7e6ffed0
Aleksey Belchenkov <u608110@gmail.com>
2020-10-11 12:39:39 +0300

    Cart reducer & add to cart action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+import {
+    CART_REMOVE_ITEM,
+    CART_ADD_ITEM
+} from "../constants/cartConstants";
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+    try {
+        const {
+            data: {
+                _id,
+                name,
+                image,
+                countInStock
+            }
+        } = await axios.get('/api/products/${id}');
+
+        dispatch({
+            type: CART_ADD_ITEM,
+            payload: {
+                product: _id,
+                name,
+                image,
+                countInStock,
+                qty
+            }
+        });
+
+        localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    } catch (error) {
+        console.log(error);
+    }
+};
