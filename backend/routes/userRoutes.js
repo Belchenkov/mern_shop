36347f01b073f588bd5834d887b966c75a087edc
Aleksey Belchenkov <u608110@gmail.com>
2020-10-14 13:31:03 +0300

    User Register & Encrypt Password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { authUser, getUserProfile } from "../controllers/userContoller.js";
+import { authUser, getUserProfile, registerUser } from "../controllers/userContoller.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.post('/', registerUser);
 router.post('/login', authUser);
 
 router.route('/profile').get(protect, getUserProfile);
