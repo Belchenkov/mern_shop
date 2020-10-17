78f93b4ea2c04cabfdb09f50d5dd4502b8bb3032
Aleksey Belchenkov <u608110@gmail.com>
2020-10-17 15:36:02 +0300

    Update User Profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -9,7 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
 
 const ProfileScreen = ({ history }) => {
     const [name, setName] = useState('');
@@ -24,6 +24,9 @@ const ProfileScreen = ({ history }) => {
 
     const userLogin = useSelector(state => state.userLogin);
     const { userInfo } = userLogin;
+
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile);
+    const { success } = userUpdateProfile;
 
     useEffect(() => {
         if (!userInfo) {
@@ -44,7 +47,12 @@ const ProfileScreen = ({ history }) => {
         if (password !== confirmPassword) {
             setMessage('Passwords do not match');
         } else {
-            // Dispatch
+            dispatch(updateUserProfile({
+                id: user._id,
+                name,
+                email,
+                password
+            }));
         }
     }
 
@@ -55,6 +63,7 @@ const ProfileScreen = ({ history }) => {
 
                 { message && <Message variant="danger">{message}</Message> }
                 { error && <Message variant="danger">{error}</Message> }
+                { success && <Message variant="success">Profile Updated</Message> }
                 { loading && <Loader /> }
 
                 <Form onSubmit={submitHandler}>
