62758445b91ad5fe1494fa591b12b407c19efa82
Aleksey Belchenkov <u608110@gmail.com>
2020-11-06 16:17:38 +0300

    Create Review Route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
     deleteProduct,
     getProductById,
     getProducts,
+    createProductReview,
     updateProduct
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
@@ -14,6 +15,7 @@ const router = express.Router();
 router.route('/')
     .post(protect, admin, createProduct)
     .get(getProducts);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/:id')
     .get(getProductById)
     .put(protect, admin, updateProduct)
